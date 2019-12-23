af0abcfa7cf3610c77b744e913204f01697135c0
Ghanna96 <akramghanna@outlook.com>
2019-12-23 16:59:50 +0100

    push to github

--- js/script.js
+++ js/script.js
@@ -1,74 +1,87 @@
-"use strict";
 
 let myLibrary = store();
 const addBook= document.querySelector("#addBook");
 const books= document.querySelector("#books");
 const popUp=document.querySelector('.popUp');
 
+// myLibrary.push(new book("The Hobbit","J.R.R. Tolkien",295,"Read"));
 //-----------------------------
 //      EVENT LISTENERS       |
 //-----------------------------
 
 //render at start
-window.addEventListener('load', () =>{ 
-    if(myLibrary!==null){ 
-        for(let i in myLibrary){
-            render(i);
-        }
-        return
-    }
-});
 //pop up form
 addBook.addEventListener('click',()=>{
     addBookToLibrary();
 });
 document.querySelector('#spawn').addEventListener("click", function() {
-	popUp.style.display = "flex";
+    popUp.style.display = "flex";
 });
 document.querySelector('#closeForm').addEventListener("click", function() {
-	popUp.style.display = "none";
+    popUp.style.display = "none";
 });
 
 //adds event that delete a book from library when pressing x
-const addEvent=(currentElement,book)=>{
-    let x=currentElement.querySelector(".close");
-    let y=currentElement.querySelector('[value="Read"],[value="Not read"]');
+window.onload= startPage();
 
+
+//-----------------------------
+//     FUNCTION SECTION       |
+//-----------------------------
+function addEvent(currentElement,book){
+    let x=currentElement.querySelector(".close");
+    let y=currentElement.querySelector('#read');
+    
     x.addEventListener("click",()=>{
         handleClose(x);
         return
     })
-
+    
     y.addEventListener("click",()=>{
         handleReading(y,book);
         return
     })
 };
-// myLibrary.push(new book("The Hobbit","J.R.R. Tolkien",295,"Read"));
-//-----------------------------
-//     FUNCTION SECTION       |
-//-----------------------------
-function store(){
-    let libStore=JSON.parse(localStorage.getItem('library'));
-    return (libStore===null)? [] : libStore
-}
-
-const updateLib=()=>{
-    localStorage.setItem('library', JSON.stringify(myLibrary));
-    return
-}
-
-const handleReading=(button,book)=>{
-    book.read= (book.read==="Read")? "Not read": "Read";
-    button.value=book.read;
-    return
+//render book in HTML
+function render(i){ 
+    let section= document.createElement("section");
+    section.innerHTML=` 
+    <ul data-index=${i}>
+        <div class="close">+</div>
+        <li class="title">${myLibrary[i].title}</li>
+        <br>
+        <li style="font-size:19px">${myLibrary[i].author}</li>
+        <br>
+        Pages:<li> ${myLibrary[i].pages}</li>
+        <br>
+        <li><input type="button" id="read" value="${myLibrary[i].read}"></li>
+    </ul>
+    `;
+    changeBackground(myLibrary[i].read,section.querySelector('#read'));
+    section.classList.add('card');
+    books.appendChild(section);
+    let node=document.querySelector(`[data-index="${i}"]`).parentElement;
+    addEvent(node,myLibrary[i]);
 };
 
-const handleClose=(x)=>{
+function handleReading(button,book){
+    book.read= (book.read==="Read")? "Not read": "Read";
+    button.value=book.read;
+    changeBackground(button.value,button)
+    return
+};
+function changeBackground(r,button){
+    if(r=="Read"){
+        button.style.backgroundColor = '#47d694';
+    }else if(r=="Not read"){
+        button.style.backgroundColor = '#b32727'; 
+    }
+}
+function handleClose(x){
     let o=x.parentNode.getAttribute('data-index');
-        x.parentNode.parentNode.remove();
-        myLibrary.splice(o,1);
-        updateLib();
+    x.parentNode.parentNode.remove();
+    myLibrary.splice(o,1);
+    updateLib();
 };
 //constructor
 function book(title,author,pages,read){
@@ -81,35 +94,13 @@ function book(title,author,pages,read){
     }
 };
 
-//render book in HTML
-const render=(i)=>{ 
-    let section= document.createElement("section");
-    section.innerHTML=` 
-    <ul data-index=${i}>
-    <div class="close">+</div>
-    <li>${myLibrary[i].title}</li>
-    <br>
-    <li>${myLibrary[i].author}</li>
-    <br>
-    Pages:<li> ${myLibrary[i].pages}</li>
-    <br>
-    <li><input type="button" value="${myLibrary[i].read}"></button></li>
-    </ul>
-     `;
-     section.classList.add('card');
-     section.classList.add('card'); 
-     section.classList.add('card');
-     books.appendChild(section);
-     let node=document.querySelector(`[data-index="${i}"]`).parentElement;
-    addEvent(node,myLibrary[i]);
-};
 
 //read the name...
-const addBookToLibrary=()=> {
+function addBookToLibrary () {
     let title=document.querySelector("#title").value,
-        author=document.querySelector("#author").value,
-        pages=document.querySelector("#pages").value,
-        read=document.querySelector('input[name="read"]:checked').value;
+    author=document.querySelector("#author").value,
+    pages=document.querySelector("#pages").value,
+    read=document.querySelector('input[name="read"]:checked').value;
     if(title== "" || author== "" || pages== ""){
         alert ("please fill all the fields");
     }else{
@@ -118,4 +109,21 @@ const addBookToLibrary=()=> {
         render(myLibrary.length-1);
         popUp.style.display = "none";
     }
+}
+
+function startPage(){
+    if(myLibrary!==null){ 
+        for(let i in myLibrary){
+            render(i);
+        }
+        return
+    }
+}
+function updateLib(){
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+    return
+}
+function store(){
+    let libStore=JSON.parse(localStorage.getItem('library'));
+    return (libStore===null)? [] : libStore
 }
